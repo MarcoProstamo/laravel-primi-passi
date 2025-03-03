afe462205346fee27e42c881375aae3f4a24780e
MarcoProstamo <Marcopros1201@gmail.com>
2025-03-03 10:26:47 +0100

    Add → Navbar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/us', function () {
+    return view('us');
+});
+
+Route::get('/potato', function () {
+    return view('potato');
+});
+
